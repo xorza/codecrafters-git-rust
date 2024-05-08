9d52bfde80cf2f9c8e6aefe36d66ada645ff4692
xxorza <xxorza@gmail.com>
2024-05-08 18:09:17 +0300

    Add 'cat-file' command and update project run configuration
    This commit introduces a new 'cat-file' command to the main rust application, which allows printing the contents of a git object. Also, the .idea/runConfigurations/blob_sha.xml has been updated to configure running of the git-starter-rust with the cat-file feature

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,51 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
+extern crate core;
+
 use std::fs;
+use std::io::{BufRead, Read};
+
+use anyhow::anyhow;
 use clap::{Arg, ArgMatches, Command};
+use flate2::read::ZlibDecoder;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     match get_matches().subcommand() {
-        Some(("init", _decode_matches)) => {
-            fs::create_dir(".git").unwrap();
-            fs::create_dir(".git/objects").unwrap();
-            fs::create_dir(".git/refs").unwrap();
-            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+        Some(("init", _init_matches)) => {
+            fs::create_dir(".git")?;
+            fs::create_dir(".git/objects")?;
+            fs::create_dir(".git/refs")?;
+            fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
             println!("Initialized git directory")
+        }
+        Some(("cat-file", cat_file_matches)) => {
+            let blob_sha = cat_file_matches
+                .get_one::<String>("blob_sha")
+                .expect("blob_sha is required");
+            if blob_sha.len() != 40 {
+                eprintln!("Invalid blob SHA: {}", blob_sha);
+                return Err(anyhow!("Invalid blob SHA: {}", blob_sha));
+            }
+            let filename = format!(".git/objects/{}/{}", &blob_sha[..2], &blob_sha[2..]);
+            let file = fs::File::open(filename)?;
+            let decoder = ZlibDecoder::new(file);
+            let mut reader = std::io::BufReader::new(decoder);
+
+            let mut buf = Vec::new();
+            reader.read_until(0, &mut buf)?;
+
+            let text = std::str::from_utf8(&buf[..buf.len() - 1])?;
+            let text = text.strip_prefix("blob ")
+                .ok_or(anyhow!("Invalid blob"))?;
+            let size: usize = text.parse()?;
+            buf.resize(size, 0);
+            reader.read_exact(&mut buf)?;
+
+            let content = std::str::from_utf8(&buf)?;
+            print!("{}", content);
         }
         _ => { eprintln!("Invalid command, use --help."); }
     }
-    
+
     Ok(())
 }
 
@@ -28,10 +57,17 @@ fn get_matches() -> ArgMatches {
         .subcommand(
             Command::new("init")
                 .about("Initialize a new git repository")
-            // .arg(
-            //     Arg::new("encoded_value")
-            //     .required(true)
-            // ),
+        )
+        .subcommand(
+            Command::new("cat-file")
+                .about("Prints the contents of a git object")
+                .arg(
+                    Arg::new("blob_sha")
+                        .short('p')
+                        .required(true)
+                        .value_name("BLOB_SHA")
+                        .help("The SHA of the blob to print"),
+                ),
         )
         .get_matches()
 }
