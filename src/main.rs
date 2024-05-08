9131ef642bd9bbadc46d401c361291204e421ca6
xxorza <xxorza@gmail.com>
2024-05-08 16:41:31 +0300

    Add basic 'init' subcommand and implement directory structure
    The 'init' subcommand has been added using the Clap crate for argument parsing. It initializes a new git repository with the correct directory structure. Also, module file (.iml) has been adjusted to reflect inclusion of 'src' as a source folder and exclusion of 'target' directory.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,36 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use clap::{Arg, ArgMatches, Command};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    match get_matches().subcommand() {
+        Some(("init", _decode_matches)) => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+        _ => { eprintln!("Invalid command, use --help."); }
+    }
+    
+    Ok(())
+}
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+fn get_matches() -> ArgMatches {
+    Command::new("Rust Git")
+        .version("0.1.0")
+        .author("xxorza")
+        .about("A simple git implementation in Rust")
+        .subcommand(
+            Command::new("init")
+                .about("Initialize a new git repository")
+            // .arg(
+            //     Arg::new("encoded_value")
+            //     .required(true)
+            // ),
+        )
+        .get_matches()
 }
