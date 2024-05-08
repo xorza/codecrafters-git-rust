fc0275e2b4a4b4a3615ca3bf3f179ac0ba5f8263
xxorza <xxorza@gmail.com>
2024-05-08 19:05:31 +0300

    Added hash-object functionality and improved file handling
    The code has been updated to add a new subcommand "hash-object", which computes an object ID and optionally creates a blob from a file. This update enhances the flexibility of the program, making it capable of handling more git-related tasks. Additionally, file handling was improved by refining the use of BufReader and introducing better error handling for invalid SHA values.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate core;
 
+use std::fmt::Write as _;
 use std::fs;
-use std::io::{BufRead, Read};
+use std::io::{BufRead, BufReader, Read, Write};
 
 use anyhow::anyhow;
-use clap::{Arg, ArgMatches, Command};
+use bytes::{BufMut, BytesMut};
+use clap::{Arg, ArgAction, ArgMatches, Command};
 use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use sha1::Digest;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -25,10 +29,10 @@ async fn main() -> anyhow::Result<()> {
                 eprintln!("Invalid blob SHA: {}", blob_sha);
                 return Err(anyhow!("Invalid blob SHA: {}", blob_sha));
             }
-            let filename = format!(".git/objects/{}/{}", &blob_sha[..2], &blob_sha[2..]);
+            let filename = filename_from_sha(&blob_sha)?;
             let file = fs::File::open(filename)?;
             let decoder = ZlibDecoder::new(file);
-            let mut reader = std::io::BufReader::new(decoder);
+            let mut reader = BufReader::new(decoder);
 
             let mut buf = Vec::new();
             reader.read_until(0, &mut buf)?;
@@ -43,6 +47,43 @@ async fn main() -> anyhow::Result<()> {
             let content = std::str::from_utf8(&buf)?;
             print!("{}", content);
         }
+        Some(("hash-object", hash_object_matches)) => {
+            let filename = hash_object_matches
+                .get_one::<String>("file")
+                .expect("file is required");
+            let should_write = hash_object_matches.get_flag("write");
+
+            let mut buf = BytesMut::new();
+            buf.write_str("blob ")?;
+            let mut input_file = fs::File::open(&filename)?;
+            let size = input_file.metadata()?.len();
+
+            buf.write_str(&size.to_string())?;
+            buf.put_u8(0);
+            let start_content = buf.len();
+            buf.resize(buf.len() + size as usize, 0);
+            input_file.read_exact(&mut buf[start_content..])?;
+
+            let mut hasher = sha1::Sha1::new();
+            hasher.update(&buf[start_content..]);
+            let blob_sha = hex::encode(hasher.finalize());
+            println!("File sha1: {}", blob_sha);
+
+            if should_write {
+                let directory = directory_from_sha(&blob_sha)?;
+                fs::create_dir_all(&directory)?;
+
+                let filename = filename_from_sha(&blob_sha)?;
+                let blob_file = fs::OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .truncate(true)
+                    .open(filename)?;
+                let mut encoder = ZlibEncoder::new(blob_file, flate2::Compression::default());
+                encoder.write(&buf)?;
+            }
+        }
+
         _ => { eprintln!("Invalid command, use --help."); }
     }
 
@@ -69,5 +110,33 @@ fn get_matches() -> ArgMatches {
                         .help("The SHA of the blob to print"),
                 ),
         )
+        .subcommand(
+            Command::new("hash-object")
+                .about("Compute object ID and optionally creates a blob from a file")
+                .arg(
+                    Arg::new("write")
+                        .short('w')
+                        .action(ArgAction::SetTrue)
+                        .help("Write the object into the object database"),
+                ).arg(
+                Arg::new("file")
+                    .value_name("FILE")
+                    .help("Read the object from the given file"),
+            )
+        )
         .get_matches()
+}
+
+fn filename_from_sha(sha: &str) -> anyhow::Result<String> {
+    if sha.len() != 40 {
+        return Err(anyhow!("Invalid SHA: {}", sha));
+    }
+    Ok(format!(".git/objects/{}/{}", &sha[..2], &sha[2..]))
+}
+
+fn directory_from_sha(sha: &str) -> anyhow::Result<String> {
+    if sha.len() != 40 {
+        return Err(anyhow!("Invalid SHA: {}", sha));
+    }
+    Ok(format!(".git/objects/{}", &sha[..2]))
 }
